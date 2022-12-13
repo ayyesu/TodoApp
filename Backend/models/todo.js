ab708583a1f7ba94a933695b1a2ffac4a6784884
Daniel Kissiedu <iamdanielkissiedu@gmail.com>
2022-12-13 11:03:30 +0000

    Testing endpoints

--- Backend/models/todo.js
+++ Backend/models/todo.js
@@ -1,22 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const todoSchema = new mongoose.Schema({
-    name: { 
-        type: String, 
-        required: true, 
-        minLength:  3, 
-        maxLength: 200
-    },
-    author: String,
-    uid: String,
-    isComplete: Boolean,
-    date: { 
-        type: Date,
-        default: new Date()
-    }
-})
+  name: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 200,
+  },
+  author: { type: String, minLength: 3, maxLength: 30 },
+  uid: String,
+  isComplete: Boolean,
+  date: {
+    type: Date,
+    default: new Date(),
+  },
+});
 
-const Todo = mongoose.model('Todo', todoSchema)
+const Todo = mongoose.model("Todo", todoSchema);
 
-exports.Todo = Todo
-
+exports.Todo = Todo;
--- Backend/routes/todos.js
+++ Backend/routes/todos.js
@@ -1,24 +1,47 @@
-const mongoose = require('mongoose')
-const { Todo } = require('../models/todo')
-const express = require('express');
+const mongoose = require("mongoose");
+const { Todo } = require("../models/todo");
+const express = require("express");
+const Joi = require("joi");
 
 const router = express.Router();
 
-router.post("/", async (req, res) => {
-    
-    const { name, author, isComplete, date, uid } = req.body;
+router.get("/", async (req, res) => {
+  try {
+    const todos = await Todo.find().sort({ date: -1 });
+    res.send(todos);
+  } catch (error) {
+    res.status(500).send(error.message);
+    console.log(error.message);
+  }
+});
 
-    let todo = new Todo({
-        name, author, isComplete, date, uid
-    })
-    try {
+router.post("/", async (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(200).required(),
+    author: Joi.string().min(3).max(30),
+    uid: Joi.string(),
+    isComplete: Joi.boolean(),
+    date: Joi.date(),
+  });
+
+  const { error } = schema.validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const { name, author, isComplete, date, uid } = req.body;
+
+  let todo = new Todo({
+    name,
+    author,
+    isComplete,
+    date,
+    uid,
+  });
+  try {
     todo = await todo.save();
-        res.send(todo);
-    } catch(error) {
-        res.status(400).send(error.message);
-    }
-})
+    res.send(todo);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
 
 module.exports = router;
-
-
