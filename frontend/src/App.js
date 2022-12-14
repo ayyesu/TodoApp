13d9d9cda095ca22d7bf1094ac021f30a3e1c177
Daniel Kissiedu <iamdanielkissiedu@gmail.com>
2022-12-14 10:36:41 +0000

    Routes created

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Todos from "./components/todos/Todos"
+import Signin from './components/auth/Signin';
+import SignUp from './components/auth/SignUp';
+import NavBar from './components/navBar/NavBar';
+
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <NavBar/>
+        <Routes>
+        <Route path="/signin" element={<Signin/>}/>
+        <Route path="/signup" element={<SignUp/>}/>
+        <Route path="/todos" element={<Todos/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
